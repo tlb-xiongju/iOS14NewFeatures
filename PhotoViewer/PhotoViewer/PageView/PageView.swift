98bd8016a33b59b400ee8d09cabb092a96f266c6
Xiong Ju <xiongju@team-lab.com>
2020-10-20 13:36:24 +0900

    there will be a widget

--- PhotoViewer/PhotoViewer/PageView/PageView.swift
+++ PhotoViewer/PhotoViewer/PageView/PageView.swift
@@ -5,4 +5,20 @@
 //  Created by 熊 炬 on 2020/10/20.
 //
 
-import Foundation
+import SwiftUI
+
+struct PageView<Page: View>: View {
+    var viewControllers: [UIHostingController<Page>]
+    @State var currentPage = 0
+    init(_ views: [Page]) {
+        self.viewControllers = views.map { UIHostingController(rootView: $0) }
+    }
+
+    var body: some View {
+        ZStack(alignment: .bottom) {
+            PageViewController(controllers: viewControllers, currentPage: $currentPage)
+            PageControl(numberOfPages: viewControllers.count, currentPage: $currentPage)
+        }
+    }
+}
+
--- PhotoViewer/PhotoViewer/PageView/PhotoView.swift
+++ PhotoViewer/PhotoViewer/PageView/PhotoView.swift
@@ -5,4 +5,11 @@
 //  Created by 熊 炬 on 2020/10/20.
 //
 
-import Foundation
+import SwiftUI
+
+struct PhotoView: View {
+    var image: UIImage
+    var body: some View {
+        Image(uiImage: image)
+    }
+}
