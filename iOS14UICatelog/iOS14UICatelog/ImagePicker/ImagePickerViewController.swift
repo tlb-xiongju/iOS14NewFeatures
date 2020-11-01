ee6a718b30fe1dc0d7981cc90e67ed24872780a4
Xiong Ju <xiongju@team-lab.com>
2020-11-01 20:28:46 +0900

    new and old pickers

--- iOS14UICatelog/iOS14UICatelog/ImagePicker/ImagePickerViewController.swift
+++ iOS14UICatelog/iOS14UICatelog/ImagePicker/ImagePickerViewController.swift
@@ -6,30 +6,51 @@
 //
 
 import UIKit
+import PhotosUI
 
 class ImagePickerViewController: UIViewController {
-    
-    @IBOutlet private weak var imagePickerButton: UIButton!
     @IBOutlet private weak var imageView: UIImageView!
+    
+    private lazy var PHPicker: PHPickerViewController = {
+        var config = PHPickerConfiguration()
+        config.filter = .images
+        config.selectionLimit = 2
+        let picker = PHPickerViewController(configuration: config)
+        picker.delegate = self
+        return picker
+    }()
     
     private lazy var imagePicker: UIImagePickerController = {
         let picker = UIImagePickerController()
-        picker.delegate = self
         picker.mediaTypes = ["public.image"]
         picker.sourceType = .photoLibrary
+        picker.delegate = self
         return picker
     }()
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
     
     @IBAction private func presentImagePicker(_ sender: UIButton) {
         present(imagePicker, animated: true)
     }
+    @IBAction private func presentPHImagePicker(_ sender: UIButton) {
+        present(PHPicker, animated: true)
+    }
 
+}
+
+extension ImagePickerViewController: PHPickerViewControllerDelegate {
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        picker.dismiss(animated: true)
+        
+        guard let result = results.first,
+              result.itemProvider.canLoadObject(ofClass: UIImage.self)
+              else { return }
+        
+        result.itemProvider.loadObject(ofClass: UIImage.self) { [self] image, error in
+            DispatchQueue.main.async {
+                self.imageView.image = image as? UIImage
+            }
+        }
+    }
 }
 
 extension ImagePickerViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
