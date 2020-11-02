79fbdac51cc9c8ce5df5c5e65d887f8eccaf882a
Xiong Ju <xiongju@team-lab.com>
2020-11-02 11:08:29 +0900

    a birds viewer

---
+++ BirdsViewer/BirdsViewer/BirdsViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirdsViewerApp.swift
+//  BirdsViewer
+//
+//  Created by 熊 炬 on 2020/11/02.
+//
+
+import SwiftUI
+
+@main
+struct BirdsViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
